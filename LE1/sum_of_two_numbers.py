e71010c578cf79c18e609493020e4d00f7e1774e
231B262 <231B262@juet.co.in>
2025-07-31 12:54:40 +0530

    Added solution to Lab Exercise 1

---
+++ LE1/sum_of_two_numbers.py
@@ -0,0 +1,11 @@
+# This program adds two numbers and prints the result
+
+# Input: take two numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the two numbers
+sum = num1 + num2
+
+# Output: print the sum
+print("The sum of", num1, "and", num2, "is", sum)
